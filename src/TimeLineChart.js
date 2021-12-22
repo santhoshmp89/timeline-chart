67bae1c6c1d3711cc75b82a2f0b76bde275097b0
Santhosh Kumar <santhoshmp89@gmail.com>
2021-12-22 17:30:59 +0530

    added nice ticks

--- src/TimeLineChart.js
+++ src/TimeLineChart.js
@@ -4,14 +4,16 @@ import * as d3 from "d3";
 const Margin = {
   top: 20,
   right: 20,
-  bottom: 20,
+  bottom: 50,
   left: 20
 };
-const SVG_WIDTH = 800 - Margin.left - Margin.right;
-const SVG_HEIGHT = 300 - Margin.top - Margin.bottom;
+const SVG_WIDTH = 800;
+const SVG_HEIGHT = 300;
+const innerWidth = SVG_WIDTH - Margin.left - Margin.right;
+const innerHeight = SVG_HEIGHT - Margin.top - Margin.bottom;
 const StartDate = new Date("Tue Dec 21 2021 00:00:00 GMT+0530");
-const EndDate = new Date("Tue Dec 21 2021 24:00:00 GMT+0530");
-const format = d3.timeFormat("%H %p");
+const EndDate = new Date("Tue Dec 21 2021 23:59:00 GMT+0530");
+const format = d3.timeFormat("%H:%M");
 
 const TimelineChart = () => {
   const svgRef = React.useRef(null);
@@ -21,7 +23,7 @@ const TimelineChart = () => {
   const xScale = d3
     .scaleTime()
     .domain([StartDate, EndDate])
-    .range([Margin.left, SVG_WIDTH - Margin.right]);
+    .range([0, innerWidth]);
 
   // const xAxisGenerator = d3
   //   .axisBottom()
@@ -37,27 +39,35 @@ const TimelineChart = () => {
 
   return (
     <svg ref={svgRef} width={SVG_WIDTH} height={SVG_HEIGHT}>
-      {xScale.ticks(24).map((tickerValue) => (
-        <g key={tickerValue} transform={`translate(${xScale(tickerValue)}, 0)`}>
-          <line x1="0" y1="0" x2="0" y2={SVG_HEIGHT - 10} stroke="#ccc" />
-          <text
-            x="0"
-            y={SVG_HEIGHT}
-            textAnchor="middle"
-            style={{ fontSize: 9 }}
-          >
-            {format(tickerValue)}
-          </text>
+      <g transform={`translate(${Margin.left}, ${Margin.right})`}>
+        {xScale
+          .nice()
+          .ticks(24)
+          .map((tickerValue) => (
+            <g
+              key={tickerValue}
+              transform={`translate(${xScale(tickerValue)}, 0)`}
+            >
+              <line x1="0" y1="0" x2="0" y2={innerHeight} stroke="#ccc" />
+              <text
+                x="0"
+                y={innerHeight + 10}
+                textAnchor="middle"
+                style={{ fontSize: 9 }}
+              >
+                {format(tickerValue)}
+              </text>
+            </g>
+          ))}
+        <g>
+          <line
+            x1={-5}
+            y1={innerHeight - 5}
+            x2={innerWidth}
+            y2={innerHeight - 5}
+            stroke="black"
+          />
         </g>
-      ))}
-      <g>
-        <line
-          x1="0"
-          y1={SVG_HEIGHT - 10}
-          x2={SVG_WIDTH}
-          y2={SVG_HEIGHT - 10}
-          stroke="black"
-        />
       </g>
     </svg>
   );
