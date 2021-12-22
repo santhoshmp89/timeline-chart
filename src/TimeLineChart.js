535ac265d17e7efc206814cca6df6923236afa4f
Santhosh Kumar <santhoshmp89@gmail.com>
2021-12-22 18:45:30 +0530

    a

--- src/TimeLineChart.js
+++ src/TimeLineChart.js
@@ -5,15 +5,41 @@ const Margin = {
   top: 20,
   right: 20,
   bottom: 50,
-  left: 20
+  left: 80
 };
-const SVG_WIDTH = 800;
+const SVG_WIDTH = 1200;
 const SVG_HEIGHT = 300;
 const innerWidth = SVG_WIDTH - Margin.left - Margin.right;
 const innerHeight = SVG_HEIGHT - Margin.top - Margin.bottom;
 const StartDate = new Date("Tue Dec 21 2021 00:00:00 GMT+0530");
 const EndDate = new Date("Tue Dec 21 2021 23:59:00 GMT+0530");
-const format = d3.timeFormat("%H:%M");
+
+const formatMillisecond = d3.timeFormat(".%L"),
+  formatSecond = d3.timeFormat(":%S"),
+  formatMinute = d3.timeFormat("%I:%M"),
+  formatHour = d3.timeFormat("%I:%M"),
+  formatDay = d3.timeFormat("%a %d"),
+  formatWeek = d3.timeFormat("%b %d"),
+  formatMonth = d3.timeFormat("%B"),
+  formatYear = d3.timeFormat("%Y");
+
+function multiFormat(date) {
+  return (d3.timeSecond(date) < date
+    ? formatMillisecond
+    : d3.timeMinute(date) < date
+    ? formatSecond
+    : d3.timeHour(date) < date
+    ? formatMinute
+    : d3.timeDay(date) < date
+    ? formatHour
+    : d3.timeMonth(date) < date
+    ? d3.timeWeek(date) < date
+      ? formatDay
+      : formatWeek
+    : d3.timeYear(date) < date
+    ? formatMonth
+    : formatYear)(date);
+}
 
 const TimelineChart = () => {
   const svgRef = React.useRef(null);
@@ -43,22 +69,33 @@ const TimelineChart = () => {
         {xScale
           .nice()
           .ticks(24)
-          .map((tickerValue) => (
-            <g
-              key={tickerValue}
-              transform={`translate(${xScale(tickerValue)}, 0)`}
-            >
-              <line x1="0" y1="0" x2="0" y2={innerHeight} stroke="#ccc" />
-              <text
-                x="0"
-                y={innerHeight + 10}
-                textAnchor="middle"
-                style={{ fontSize: 9 }}
+          .map((tickerValue, i) => {
+            const strokeColor = i === 0 ? "black" : "#ccc";
+            return (
+              <g
+                key={tickerValue}
+                transform={`translate(${xScale(tickerValue)}, 0)`}
               >
-                {format(tickerValue)}
-              </text>
-            </g>
-          ))}
+                <line
+                  x1="0"
+                  y1="0"
+                  x2="0"
+                  y2={innerHeight}
+                  stroke={strokeColor}
+                />
+                {i % 4 === 0 && (
+                  <text
+                    x="0"
+                    y={innerHeight + 10}
+                    textAnchor="middle"
+                    style={{ fontSize: 9 }}
+                  >
+                    {multiFormat(tickerValue)}
+                  </text>
+                )}
+              </g>
+            );
+          })}
         <g>
           <line
             x1={-5}
