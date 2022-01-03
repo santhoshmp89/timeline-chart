7c4844a339f1ed309528ac98198640a15dd1f569
Santhosh Kumar <santhoshmp89@gmail.com>
2022-01-03 12:32:44 +0530

    Yaxis changes

--- src/TimeLineChart.js
+++ src/TimeLineChart.js
@@ -1,6 +1,38 @@
 import React from "react";
 import * as d3 from "d3";
 
+const data = [
+  {
+    name: "Alerts & Events",
+    metrics: [
+      { date: new Date("Tue Dec 21 2021 10:00:00 GMT+0530") },
+      { date: new Date("Tue Dec 21 2021 20:00:00 GMT+0530") },
+      { date: new Date("Tue Dec 21 2021 08:00:00 GMT+0530") },
+      { date: new Date("Tue Dec 21 2021 08:30:00 GMT+0530") }
+    ]
+  },
+  {
+    name: "VPN Connection",
+    metrics: [
+      { date: new Date("Tue Dec 21 2021 11:00:00 GMT+0530") },
+      { date: new Date("Tue Dec 21 2021 23:00:00 GMT+0530") },
+      { date: new Date("Tue Dec 21 2021 18:00:00 GMT+0530") },
+      { date: new Date("Tue Dec 21 2021 23:30:00 GMT+0530") }
+    ]
+  },
+  {
+    name: "Online",
+    metrics: [
+      { date: new Date("Tue Dec 21 2021 09:00:00 GMT+0530") },
+      { date: new Date("Tue Dec 21 2021 01:00:00 GMT+0530") },
+      { date: new Date("Tue Dec 21 2021 03:00:00 GMT+0530") },
+      { date: new Date("Tue Dec 21 2021 04:30:00 GMT+0530") }
+    ]
+  }
+];
+
+const getYaxisLabel = ["Alerts", "VPN Connection", "Online"];
+
 const Margin = {
   top: 20,
   right: 20,
@@ -43,34 +75,38 @@ function multiFormat(date) {
 
 const TimelineChart = () => {
   const svgRef = React.useRef(null);
+  const yAxis = React.useRef(null);
   // const svgElement = d3.select(svgRef.current);
-  React.useEffect(() => {}, []);
-
   const xScale = d3
     .scaleTime()
     .domain([StartDate, EndDate])
     .range([0, innerWidth]);
 
-  // const xAxisGenerator = d3
-  //   .axisBottom()
-  //   .scale(xScale)
-  //   .ticks(d3.timeHour.every(4));
-
-  // if (svgElement) {
-  //   svgElement
-  //     .append("g")
-  //     .attr("transform", "translate(0, " + SVG_HEIGHT + ")")
-  //     .call(xAxisGenerator);
-  // }
+  const yScale = d3
+    .scalePoint()
+    .domain(getYaxisLabel)
+    .range([0, innerHeight])
+    .padding(`0.${getYaxisLabel.length + 1}`);
+  console.log(innerHeight);
+  React.useEffect(() => {
+    const yAxisGenerator = d3
+      .axisLeft()
+      .scale(yScale)
+      .tickPadding([10])
+      .tickSizeInner([10])
+      .tickSize(0);
+    d3.select(yAxis.current).call(yAxisGenerator);
+  }, [data]);
 
   return (
     <svg ref={svgRef} width={SVG_WIDTH} height={SVG_HEIGHT}>
       <g transform={`translate(${Margin.left}, ${Margin.right})`}>
+        <g ref={yAxis} transform={`translate(0, 0)`}></g>
         {xScale
           .nice()
           .ticks(24)
           .map((tickerValue, i) => {
-            const strokeColor = i === 0 ? "black" : "#ccc";
+            const strokeColor = i === 0 ? "white" : "#ccc";
             return (
               <g
                 key={tickerValue}
