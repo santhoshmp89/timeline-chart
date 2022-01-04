9365040b49ef0f10f7ce799221109a5c1a73b92a
Santhosh Kumar <santhoshmp89@gmail.com>
2022-01-04 10:25:50 +0530

    chnages

--- src/TimeLineChart.js
+++ src/TimeLineChart.js
@@ -5,33 +5,36 @@ const data = [
   {
     name: "Alerts & Events",
     values: [
-      { date: new Date("Tue Dec 21 2021 10:00:00 GMT+0530") },
-      { date: new Date("Tue Dec 21 2021 20:00:00 GMT+0530") },
-      { date: new Date("Tue Dec 21 2021 08:00:00 GMT+0530") },
-      { date: new Date("Tue Dec 21 2021 08:30:00 GMT+0530") }
+      { type: "wifi", time: new Date("Tue Dec 21 2021 10:00:00 GMT+0530") },
+      { type: "wifi", time: new Date("Tue Dec 21 2021 20:10:00 GMT+0530") },
+      { type: "wifi", time: new Date("Tue Dec 21 2021 08:00:00 GMT+0530") },
+      { type: "wifi", time: new Date("Tue Dec 21 2021 08:30:00 GMT+0530") }
     ]
   },
   {
     name: "VPN Connection",
     values: [
-      { date: new Date("Tue Dec 21 2021 11:00:00 GMT+0530") },
-      { date: new Date("Tue Dec 21 2021 23:00:00 GMT+0530") },
-      { date: new Date("Tue Dec 21 2021 18:00:00 GMT+0530") },
-      { date: new Date("Tue Dec 21 2021 23:30:00 GMT+0530") }
+      { type: "circle", time: new Date("Tue Dec 21 2021 01:00:00 GMT+0530") },
+      { type: "circle", time: new Date("Tue Dec 21 2021 23:00:00 GMT+0530") },
+      { type: "circle", time: new Date("Tue Dec 21 2021 18:00:00 GMT+0530") },
+      {
+        type: "squareWithError",
+        time: new Date("Tue Dec 21 2021 23:30:00 GMT+0530")
+      }
     ]
   },
   {
     name: "Online",
     values: [
-      { date: new Date("Tue Dec 21 2021 09:00:00 GMT+0530") },
-      { date: new Date("Tue Dec 21 2021 01:00:00 GMT+0530") },
-      { date: new Date("Tue Dec 21 2021 03:00:00 GMT+0530") },
-      { date: new Date("Tue Dec 21 2021 04:30:00 GMT+0530") }
+      { type: "online", time: new Date("Tue Dec 21 2021 09:00:00 GMT+0530") },
+      { type: "online", time: new Date("Tue Dec 21 2021 09:05:10 GMT+0530") },
+      { type: "online", time: new Date("Tue Dec 21 2021 09:10:10 GMT+0530") },
+      { type: "online", time: new Date("Tue Dec 21 2021 04:30:00 GMT+0530") }
     ]
   }
 ];
 
-const getYaxisLabel = ["Alerts & Events", "VPN Connection", "Online"];
+const getYaxisLabel = data.map((i) => i.name);
 
 const Margin = {
   top: 20,
@@ -39,8 +42,9 @@ const Margin = {
   bottom: 50,
   left: 80
 };
+
 const SVG_WIDTH = 1200;
-const SVG_HEIGHT = 300;
+const SVG_HEIGHT = 180;
 const innerWidth = SVG_WIDTH - Margin.left - Margin.right;
 const innerHeight = SVG_HEIGHT - Margin.top - Margin.bottom;
 const StartDate = new Date("Tue Dec 21 2021 00:00:00 GMT+0530");
@@ -49,7 +53,7 @@ const EndDate = new Date("Tue Dec 21 2021 23:59:00 GMT+0530");
 const formatMillisecond = d3.timeFormat(".%L"),
   formatSecond = d3.timeFormat(":%S"),
   formatMinute = d3.timeFormat("%I:%M"),
-  formatHour = d3.timeFormat("%I:%M"),
+  formatHour = d3.timeFormat("%H:%M"),
   formatDay = d3.timeFormat("%a %d"),
   formatWeek = d3.timeFormat("%b %d"),
   formatMonth = d3.timeFormat("%B"),
@@ -76,7 +80,6 @@ function multiFormat(date) {
 const TimelineChart = () => {
   const svgRef = React.useRef(null);
   const yAxis = React.useRef(null);
-  // const svgElement = d3.select(svgRef.current);
   const xScale = d3
     .scaleTime()
     .domain([StartDate, EndDate])
@@ -86,29 +89,80 @@ const TimelineChart = () => {
     .scalePoint()
     .domain(getYaxisLabel)
     .range([0, innerHeight])
-    .padding(`0.${getYaxisLabel.length + 1}`);
-
-  console.log(innerHeight);
+    .padding(`0.${getYaxisLabel.length + 4}`);
 
   React.useEffect(() => {
     const yAxisGenerator = d3.axisLeft().scale(yScale).tickSize(0);
     d3.select(yAxis.current).call(yAxisGenerator);
-  }, [data]);
+  }, [yScale]);
+
+  const getIcon = (type, iconXScale, iconYScale) => {
+    switch (type) {
+      case "wifi":
+        return (
+          <rect
+            fill="red"
+            x={iconXScale}
+            y={iconYScale}
+            width="10"
+            height="10"
+          />
+        );
+      case "circle":
+        return (
+          <circle fill="orange" cx={iconXScale} cy={iconYScale + 4} r="4" />
+        );
+      case "squareWithError":
+        return (
+          <g transform={`translate(${iconXScale} ${-iconYScale}`}>
+            <rect
+              x={0}
+              y={iconYScale}
+              width="8"
+              height="8"
+              style={{
+                fill: "#CC4643",
+                stroke: "#CC4643",
+                strokeWidth: 1,
+                fillOpacity: 0.2,
+                transform: "rotate(0deg)"
+              }}
+            />
+          </g>
+        );
+      case "online":
+        return (
+          <rect
+            fill="skyblue"
+            x={iconXScale}
+            y={iconYScale}
+            width="2"
+            height="10"
+          />
+        );
+      default:
+        return (
+          <rect
+            fill="black"
+            x={iconXScale}
+            y={iconYScale}
+            width="10"
+            height="10"
+          />
+        );
+    }
+  };
 
   const plotValues = () => {
     const values = [];
-
     for (const item of data) {
       for (const metric of item.values) {
-        values.push(
-          <rect
-            fill="black"
-            x={xScale(metric.date)}
-            y={yScale(item.name)}
-            width="3"
-            height="3"
-          />
+        const icon = getIcon(
+          metric.type,
+          xScale(metric.time),
+          yScale(item.name) - 5
         );
+        values.push(icon);
       }
     }
 
@@ -142,7 +196,7 @@ const TimelineChart = () => {
                       x="0"
                       y={innerHeight + 10}
                       textAnchor="middle"
-                      style={{ fontSize: 9 }}
+                      style={{ fontSize: 12, fill: "#666" }}
                     >
                       {multiFormat(tickerValue)}
                     </text>
@@ -156,7 +210,7 @@ const TimelineChart = () => {
               y1={innerHeight - 5}
               x2={innerWidth}
               y2={innerHeight - 5}
-              stroke="black"
+              stroke="#A6A6A6"
             />
           </g>
         </g>
